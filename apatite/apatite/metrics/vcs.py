397be76f22b4cb07a48adbd3e97909d5f72de22c
Mahmoud Hashemi <mahmoud@hatnote.com>
2019-08-10 15:16:35 -0700

    sketching on vcs

--- apatite/apatite/metrics/vcs.py
+++ apatite/apatite/metrics/vcs.py
@@ -2,4 +2,16 @@
 required_cmds = []
 
 def collect(project, repo_dir):
-    return {'vcs_name': project.clone_info[0]}
+    """
+    TODO:
+
+    first_commit_date   "git rev-list --max-parents=0 HEAD"
+    last_commit_date
+    committer_count   "git shortlog --summary --numbered --email"   # might need to be deduped by name
+    commit_count
+    committer_dist    // fixed buckets, proportionalized to commit_count (# of committers that did 90% of commits, 80%, 70%, etc.)
+    """
+    vcs_name = project.clone_info[0]
+    ret = {'vcs_name': vcs_name}
+    if vcs_name != 'git':
+        return ret

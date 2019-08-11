3a26c8f082a5b1e21df1e86cf66ca5895c4a74c2
Shawn Shojaie <shawn.shojaie@gmail.com>
2019-08-10 18:13:30 -0700

    Add number of detected licenses and truncation
    Truncates the number of possible licenses to 3 if there are more than 3.
    Add refactors to remove try except blocks and replace with glom.

--- apatite/apatite/metrics/license.py
+++ apatite/apatite/metrics/license.py
@@ -1,4 +1,5 @@
 import datetime
+import glom
 import json
 import subprocess
 import time
@@ -15,20 +16,21 @@ required_cmds = {'name': CMD_NAME, 'instructions': instr}
 def collect(project, repo_dir):
 
     started = datetime.datetime.utcnow()
-    proc = subprocess.Popen([CMD_NAME, repo_dir, '--format', 'json'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    proc = subprocess.Popen([CMD_NAME, repo_dir, '--format', 'json'],
+                            stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     stdout, stderr = proc.communicate()
     proc_res = ProcessResult(returncode=proc.returncode,
                              stdout=stdout.decode('utf8'),
                              stderr=stderr,
                              start_time=started,
                              end_time=datetime.datetime.utcnow())
-    possible_licenses = json.loads(proc_res.stdout)
-    try:
-        lic = possible_licenses[0]['matches'][0]['license']
-    except KeyError:
-        lic = possible_licenses[0]['error']
-    return {'license': lic}
+    output_json = json.loads(proc_res.stdout)
+    possible_licenses = glom.glom(output_json, glom.Coalesce('0.matches'), default=[])
+    # sort and set into descending order
+    possible_licenses = sorted(possible_licenses, key=lambda x: x['confidence'] * -1)
+    num_possible_licenses = len(possible_licenses)
 
-# TOOD
-# add top three licenses and confidences
-# add total licenses detected as number
+    if num_possible_licenses > 3:
+        possible_licenses = possible_licenses[:3]
+
+    return {'licenses': possible_licenses, 'total_detected_licenses': num_possible_licenses}

0861a4883ff9f65f89e00ee0120c78338491d481
Shawn Shojaie <shawn.shojaie@gmail.com>
2019-08-10 17:13:41 -0700

    Add rough license detection module

---
+++ apatite/apatite/metrics/license.py
@@ -0,0 +1,30 @@
+import datetime
+import json
+import subprocess
+import time
+from apatite.cli import ProcessResult
+
+instr = ('Download from: https://github.com/src-d/go-license-detector/releases,',
+         ' rename to "license-detector", place it on your PATH,'
+         ' and make it executable.')
+
+CMD_NAME = 'license-detector'
+
+required_cmds = {'name': CMD_NAME, 'instructions': instr}
+
+def collect(project, repo_dir):
+
+    started = datetime.datetime.utcnow()
+    proc = subprocess.Popen([CMD_NAME, repo_dir, '--format', 'json'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = proc.communicate()
+    proc_res = ProcessResult(returncode=proc.returncode,
+                             stdout=stdout.decode('utf8'),
+                             stderr=stderr,
+                             start_time=started,
+                             end_time=datetime.datetime.utcnow())
+    possible_licenses = json.loads(proc_res.stdout)
+    try:
+        lic = possible_licenses[0]['matches'][0]['license']
+    except KeyError:
+        lic = possible_licenses[0]['error']
+    return {'license': lic}

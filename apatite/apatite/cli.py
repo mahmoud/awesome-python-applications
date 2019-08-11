e3d5cfba72d58cb901a5914b5fd5bb765710dfc4
Mahmoud Hashemi <mahmoud@hatnote.com>
2019-08-11 00:54:19 -0700

    git vcs stats basically complete, look decent

--- apatite/apatite/cli.py
+++ apatite/apatite/cli.py
@@ -388,22 +388,25 @@ def collect_metrics(plist, repo_dir, metrics_dir, targets=None, metrics=None):
 
     print('collecting data for metrics: %s' % ', '.join([m.__name__ for m in metric_mods]))
     for metric_mod in metric_mods:
-        with tqdm(total=len(project_repo_map)) as cur_metric_progress:
+        with tqdm(total=len(project_repo_map), unit='run') as cur_metric_progress:
             for project, (target_repo_dir, last_pulled) in project_repo_map.items():
-                cur_metric_progress.set_description('%s: %s' % (metric_mod.__name__, project.name_slug))
+                cur_metric_progress.set_description(f'{metric_mod.__name__}: {project.name_slug}'.ljust(20))
                 start_time = time.time()
                 try:
                     res = metric_mod.collect(project, target_repo_dir)
+                except KeyboardInterrupt:
+                    return
                 except Exception as e:
                     print_err('%s metric failed on project %s with exception %r. (run with APATITE_DEBUG=1 to debug.)'
                               % (metric_mod.__name__, project.name_slug, e))
                     if os.getenv('APATITE_DEBUG'):
                         import pdb;pdb.post_mortem()
+                    continue
                 entry = {'project': project.name_slug,
                          'metric_name': metric_mod.__name__,
                          'result': res,
                          'pull_date': last_pulled.isoformat(),
-                         '~duration': round((time.time() - start_time) * 1000, 2)}
+                         '~ms': round((time.time() - start_time) * 1000, 2)}
 
                 res_json = json.dumps(entry, sort_keys=True)
                 with open(os.path.join(metrics_dir, res_fn), 'a') as f:
--- apatite/apatite/metrics/vcs.py
+++ apatite/apatite/metrics/vcs.py
@@ -1,4 +1,10 @@
 
+import re
+import subprocess
+from collections import Counter
+
+from boltons.timeutils import isoparse
+
 required_cmds = []
 
 def collect(project, repo_dir):
@@ -13,5 +19,103 @@ def collect(project, repo_dir):
     """
     vcs_name = project.clone_info[0]
     ret = {'vcs_name': vcs_name}
-    if vcs_name != 'git':
-        return ret
+    if vcs_name == 'git':
+        ret.update(get_git_info(repo_dir))
+    return ret
+
+
+
+def run_cap(args, **kw):
+    kw['stdout'] = subprocess.PIPE
+    kw['stderr'] = subprocess.PIPE
+    kw.setdefault('encoding', 'utf8')
+    kw.setdefault('errors', 'replace')
+
+    return subprocess.run(args, **kw)
+
+
+def _get_commit_dt(repo_dir, commit_hash, **kw):
+    kw.setdefault('env', {})['TZ'] = 'UTC'
+    kw['cwd'] = repo_dir
+    proc_res = run_cap(['git', 'show', '-s', '--format=%cd', '--date=format-local:%Y-%m-%dT%H:%M:%S', commit_hash], **kw)
+    date_text = proc_res.stdout.strip()
+    return isoparse(date_text)
+
+
+_git_committer_re = re.compile(r'^\s+(?P<commit_count>\d+)\s+(?P<name>.*)\s<(?P<email>[^>]*)>$', re.MULTILINE | re.UNICODE)
+
+def get_git_info(repo_dir):
+    ret = {}
+
+    proc_res = run_cap(['git', 'rev-list', '--max-parents=0', 'HEAD'], cwd=repo_dir)
+    first_commit_hashes = proc_res.stdout.strip().split()
+
+    first_commit_dt = sorted([_get_commit_dt(repo_dir, fch) for fch in first_commit_hashes])[0]
+
+    proc_res = run_cap(['git', 'rev-parse', 'HEAD'], cwd=repo_dir)
+    latest_commit_hash = proc_res.stdout.strip()
+
+    latest_commit_dt = _get_commit_dt(repo_dir, latest_commit_hash)
+
+    ret['first_commit'] = first_commit_dt.isoformat()
+    ret['latest_commit'] = latest_commit_dt.isoformat()
+
+    proc_res = run_cap(['git', 'shortlog', '--summary', '--numbered', '--email'], cwd=repo_dir)
+
+    _commits_by_name = Counter()
+    _commits_by_email = Counter()
+    for match in _git_committer_re.finditer(proc_res.stdout):
+        gdict = match.groupdict()
+        gdict['commit_count'] = int(gdict['commit_count'])
+        _commits_by_name[gdict['name']] += gdict['commit_count']
+        _commits_by_email[gdict['email']] += gdict['commit_count']
+
+
+    ret['commit_count'] = commit_count = sum(_commits_by_name.values())
+    ret['committer_count'] = len(_commits_by_name)
+    ret['committer_email_count'] = len(_commits_by_email)
+
+    threshes = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.95, 0.99, 1.0]
+    commit_thresh_map = {thresh: (commit_count * thresh) for thresh in threshes}
+
+    # the following calculates how many developers had more than X0% of commits
+    #  - not super useful bc there'll only ever be 1 above 0.5, and squashes the info in lower thresholds.
+    # might be more interesting as fixed numbers "how many made more than 2, 5, 10, 20, 100 commits, etc."
+    # committer_dist_map = {thresh: len([x for x in _commits_by_name.values() if x > thresh_commit_count])
+    #                       for thresh, thresh_commit_count in commit_thresh_map}
+
+    sorted_committers = sorted(_commits_by_name.items(), reverse=True, key=lambda x: x[1])
+    def _get_proportion_count(thresh_commit_count):
+        _cur_commit_count = 0
+        _cur_committer_count = 0
+        for committer, committer_commit_count in sorted_committers:
+            if _cur_commit_count > thresh_commit_count:
+                break
+            _cur_commit_count += committer_commit_count
+            _cur_committer_count += 1
+        return _cur_committer_count
+
+    committer_dist_map = {thresh: _get_proportion_count(thresh_commit_count)
+                          for thresh, thresh_commit_count in commit_thresh_map.items()}
+    ret['committer_dist'] = committer_dist_map
+    ret['top_5'] = [round(c / commit_count, 4) for _, c in sorted_committers][:5]
+    ret['minor_committer_counts'] = {x: len([c for _, c in sorted_committers if c <= x])
+                                     for x in range(1, 6)}
+    """
+    how many developers's commits together comprise X0% of the commits?
+
+    vs
+
+    how many developers had more than X0% of commits
+    """
+
+    '''
+    # DEBUG
+    print(first_commit_dt.isoformat(), latest_commit_dt.isoformat(), latest_commit_dt - first_commit_dt)
+    from pprint import pprint
+    pprint(committer_dist_map)
+    pprint(ret['top_5'])
+    pprint(ret)
+    raise SystemExit
+    '''
+    return ret
